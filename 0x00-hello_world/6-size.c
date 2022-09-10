c6ffa27c8555cf5ba30d01b5245cf9358defb4ef
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-10 04:36:24 -0700

    Add program to show sizes of files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program prints out datatype sizes
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int x;
+	long long int y;
+	float z;
+
+	printf("Size of a char is: %d byte(s)\n", sizeof(c));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(x));
+	printf("Size of long long int: %d byte(s)\n", sizeof(y));
+	printf("Size of a float: %d byte(s)\n", sizeof(z));
+	return (0);
+}
