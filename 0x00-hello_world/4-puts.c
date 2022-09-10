f4141ffef8de644421478a6a51066789ee8208bb
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-10 04:08:03 -0700

    Add description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ *
+ *  * Program prints out text using puts
+ *
+ *   */
 int main(void)
 {
-	char Alx[] = "\"Programming is like building a multilingual puzzle\n";
+	char Alx[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Alx);
 	return (0);
