3c78da48d393408c1defd7614023e08da30bb920
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-12 03:05:32 -0700

    Add program

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints numbers from 0 to 9 comma seperated
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		putchar(',');
+		putchar(32);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
