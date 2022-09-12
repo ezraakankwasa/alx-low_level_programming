2e7870a3b997b6622df73674367954f497cd9568
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-11 23:37:02 -0700

    Add program to print lower and and upper case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints whether number is greater than 0
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 'A';
+
+	while (x < '{')
+	{
+		if (x >= '[' && x <= '`')
+		{
+			continue
+		}
+		putchar(x);
+		x++;
+	}
+	while (
+	putchar('\n');
+	return (0);
+}
