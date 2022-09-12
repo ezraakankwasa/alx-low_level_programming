a52037b6b0d6fdb53a59ed015f45cdf78894f26d
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-11 23:22:57 -0700

    Add program to print alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints whether number is greater than 0
+ * Return: 0
+ */
+int main(void)
+{
+	int x  = 'a'
+
+	while (x < '{')
+	{
+		putchar(c);
+		x++;
+	}
+	return (0);
+}
