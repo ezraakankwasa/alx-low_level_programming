6f1db1e2e98b8bf8e4707667e3ae5163e241c40b
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-12 01:53:58 -0700

    Fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Program prints whether number is greater than 0
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
