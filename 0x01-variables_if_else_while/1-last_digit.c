44d3ab308fffafde86cfe21f65b3052b77dee8d1
ezraakankwasa <84581520+ezraakankwasa@users.noreply.github.com>
2022-09-12 08:51:00 +0300

    Replace variables in the c program
    Changing variables with x to n%10. Trying to get rid of errors in the checker.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,22 +8,21 @@
 int main(void)
 {
 	int n;
-	int x = n%10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (x > 5)
+	if (n%10 > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n%10);
 	}
-	else if (x < 0 && x < 6)
+	else if (n%10 < 0 && n%10 < 6)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
 	}
-	else
+	else if (n%10 == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, x);
+		printf("Last digit of %d is %d and is 0\n", n, n%10);
 	}
 	return (0);
 }
