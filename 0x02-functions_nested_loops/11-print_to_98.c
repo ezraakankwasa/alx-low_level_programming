38b098ecce11523d5f0a60d3ac54100f52878ecc
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-14 02:59:05 -0700

    Fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - print numbers from n to 98
+ * @n: value to be passed in function
+ *
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		_putchar('0' + n);
+		_putchar(44);
+		_putchar(32);
+	}
+}
