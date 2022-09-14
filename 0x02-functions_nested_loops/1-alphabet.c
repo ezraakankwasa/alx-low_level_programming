52caf4bb0baf2eec261c148529d06b44274d408b
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-14 01:00:49 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,9 @@
 #include "main.h"
 
 /**
- * main - entry point for print_alphabet
+ * print_alphabet - entry point for print_alphabet
  * Return: 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 void print_alphabet(void)
 {
 	int i = 97;
