a9c31258f37260c5cfc6d40bd83e8878e8da5bc2
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-14 01:03:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,33 +1,9 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_alphabet - print lowercase alphabet
+ * print_alphabet_x10 - print lowercase alphabet
  * Return: On success 0.
  */
-void print_alphabet_x10(void);
-
-/**
- * main - entry point for print_alphabet
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
 void print_alphabet_x10(void)
 {
 	int i = 97;
