c03d29c5bbd7e24b24e27503d68b63f5c2a57822
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-17 03:08:47 -0700

    Add func to multiply two params

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - checks for digit
+ * @a: input character to be checked
+ * @b: input character to be checked
+ * Return: multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	return (mul);
+}
