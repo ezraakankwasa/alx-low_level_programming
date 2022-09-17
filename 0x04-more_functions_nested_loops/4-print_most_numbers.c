965332e380a8483a2762901be2a23723b03e4a64
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-17 03:26:45 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ * except 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+			i++;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	_putchar(10);
+}
