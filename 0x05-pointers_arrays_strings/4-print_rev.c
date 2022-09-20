dd21e28abf20e7258b098f30a058c966905d5aa2
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-20 03:07:11 -0700

    Add strlen code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,3 +14,18 @@ void print_rev(char *s)
 	}
 	_putchar(10);
 }
+
+/**
+ * _strlen - return length of string.
+ * @s: Address of string
+ *
+ * Return: length of string.
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; ++count);
+
+	return (count);
+}
