c5a666e4ce603249096f644e66c9aa76d8bc3a8c
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-22 00:32:00 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string.
+ * @str: pointer to string.
+ */
+
+void puts_half(char *str)
+{
+	int i, half, length;
+
+	half = 0;
+	length = 0;
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+	if (length % 3 == 0)
+	{
+		half = (length - 1) / 2;
+	}
+	else
+	{
+		half = length / 2;
+	}
+
+	while (half < length)
+	{
+		_putchar(*(str + half));
+		half++;
+	}
+	_putchar(10);
+}
