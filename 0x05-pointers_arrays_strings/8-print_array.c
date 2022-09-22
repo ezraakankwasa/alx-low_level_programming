6ae950a35b7f374d489a6653e5708b8102a8c7d5
ezraakankwasa <ezraakankwasamark@gmail.com>
2022-09-22 02:03:33 -0700

    Replace putchar with printf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,60 +6,20 @@
  * @n: number of items to printed out.
  */
 
-void print_array(int*a, int n)
+void print_array(int *a, int n)
 {
-	int i =0;
-	int j;
+	int i = 0;
 
 	while (i < n)
 	{
-		j = *(a + i);
-
-		if (j < 0)
-		{
-			_putchar(45);
-			j = j * -1;
-		}
-
-		if ( j > 1000)
-		{
-			_putchar('0' + (j / 1000));
-			if ((j / 100) % 10 == 0)
-			{
-				_putchar('0' + 0);
-			}
-
-			j = j % 1000;
-		}
-
-		if (j > 100)
-		{
-			_putchar('0' + (j / 100));
-			if ((j / 10) % 10 == 0)
-			{
-				_putchar('0' + 0);
-			}
-
-			j = j % 100;
-		}
-
-		if (j > 9)
-		{
-			_putchar('0' + (j / 10));
-			j = j % 10;
-		}
-
-		_putchar('0' + j);
-
+		printf("%d", *(a + i));
 		if (i == n - 1)
 		{
 			break;
 		}
-
-		_putchar(44);
-		_putchar(32);
-
+		printf(", ");
 		i += 1;
 	}
-	_putchar(10);
+
+	printf("\n");
 }
