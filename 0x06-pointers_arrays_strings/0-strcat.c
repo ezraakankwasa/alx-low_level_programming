145dffcb57dda12bb88b26f8f7bf588a462ab6d4
ezraakankwasa <84581520+ezraakankwasa@users.noreply.github.com>
2022-10-13 11:08:18 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - combines two strings together
- * @dest: destination of character to be copied
- * @src: string to be added to dest
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
  */
-
 char *_strcat(char *dest, char *src)
 {
+	int dlen = 0, i;
 
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = src[i];
+		dlen++;
+	}
+
+	dest[dlen] = '\0';
+	return (dest);
 }
