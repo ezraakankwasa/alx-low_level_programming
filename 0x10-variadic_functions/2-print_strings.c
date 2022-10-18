f4f6eea2d9a690fb8eac9fbc7dd6d92d6693b045
ezraakankwasa <84581520+ezraakankwasa@users.noreply.github.com>
2022-10-18 16:33:10 +0300

    Update 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,23 +1,33 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - calculates the sum of all its parameters
- * @n: number of arguments passed to the function
- *
- * Return: the resulting sum
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
  */
-int sum_them_all(const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum = 0;
+	char *str;
+
 	va_list list;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, int);
+	{
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
+		else
+			printf("%s%s", separator, str);
+	}
+
+	printf("\n");
 
 	va_end(list);
-
-	return (sum);
 }
