302cb688d7d172a9715fe5ecafcc7fa878a541fe
ezraakankwasa <84581520+ezraakankwasa@users.noreply.github.com>
2022-10-18 14:25:26 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,25 @@
 #include "main.h"
 
 /**
- * puts2 - print string to stdo
- * @str: Address of string
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
  */
 void puts2(char *str)
 {
-	int i;
+	int len, i;
 
-	for (i = 0; *(str + i) != '\0'; i += 2)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(*(str + i));
+		len++;
 	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
